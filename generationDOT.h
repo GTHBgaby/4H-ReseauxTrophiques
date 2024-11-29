9ff8fe18221a40e7156a4cc06f504654eb7686ea
Gabriel BESSON <gabriel.besson@edu.ece.fr>
2024-11-29 12:37:52 +0100

    Fonction modifiction complète, manque pondération et taux d'accroissement sur graph, ajout CMakeLists pour règler précédent problème.

--- generationDOT.h
+++ generationDOT.h
@@ -1,15 +1,19 @@
 #ifndef INC_4E_RESEAUXTROPHIQUES_GENERATIONDOT_H
 #define INC_4E_RESEAUXTROPHIQUES_GENERATIONDOT_H
 
+#define MAX_ESPECES 100
+#define MAX_LIGNE 256
+
 typedef struct {
-    char* nom;
-    char* couleur;
-    int niveau;
+    char nom[100];
     int population;
     float taux_accroissement;
 } Espece;
 
+void lire_fichier_dot(const char* nom_fichier, Espece* especes, int* nb_especes);
+int modifier_espece(Espece* especes, int nb_especes, const char* nom_espece);
+void mettre_a_jour_fichier_dot(const char* nom_fichier, Espece* especes, int nb_especes);
+
 void generer_dot(Espece* especes, int nb_especes, int (*relations)[2], int nb_relations, const char* filename);
-void modifier_espece(Espece* espece);
 
 #endif //INC_4E_RESEAUXTROPHIQUES_GENERATIONDOT_H
