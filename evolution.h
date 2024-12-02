8b1bed534c3a591349d7e8b0ebda22952acaab8b
titouanmtz <titouan.martinezsegui@edu.ece.fr>
2024-12-02 18:50:32 +0100

    Creation evolution. evolution.h + CMAKE

---
+++ evolution.h
@@ -0,0 +1,13 @@
+#ifndef INC_4E_RESEAUXTROPHIQUES_EVOLUTION_H
+#define INC_4E_RESEAUXTROPHIQUES_EVOLUTION_H
+#include "Graph.h"
+#include "stdlib.h"
+
+
+
+void miseAJourPopulations(Graph* graph);
+void modifierPopulation(Graph* graph, int id, double nouvelle_population);
+
+
+
+#endif //INC_4E_RESEAUXTROPHIQUES_EVOLUTION_H
